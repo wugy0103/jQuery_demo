fe8c93f7eb630c0db835e9db59e8408ff7d9b880
wuguoyuan <946296536@qq.com>
2017-03-04 14:00:17 +0800

    直播够物差排序

--- liveShopping/js/common.js
+++ liveShopping/js/common.js
@@ -33,7 +33,7 @@ if(isIOS()){
     });
 }
 //调用app方法
-function callApp(func,json,callback){
+function H5callApp(func,json,callback){
     if(isIOS()){
         connectWebViewJavascriptBridge(function (bridge) {
             bridge.callHandler(func, json, function (responedata) {
@@ -43,14 +43,16 @@ function callApp(func,json,callback){
             });
         });
     }else if(window.android) {
-        console.info("android")
         window.android[func](JSON.stringify(json));
+        if(callback){
+            callback();
+        }
     }else{
         alert("请在健康猫APP打开！");
     }
 }
-//app调用h5--有bug
-function appCallH5(func){
+//app调用h5
+function IOSCallH5(func){
     if (isIOS()) {
         connectWebViewJavascriptBridge(function (bridge) {
             bridge.registerHandler(func, function (data, responseCallback) {
@@ -83,109 +85,76 @@ function setCookie(c_name, value,domain, expiredays) {
     document.cookie = c_name + "=" + escape(value) + ";domain="+domain+";path=/" +
         ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString())
 }
-//mui分页加载
-/*function ajaxSuccess (url,type,params,callback,muiCallback,needPage) {
-    $.ajax({
-        url: url,
-        type: type,
-        data: params,
-        dataType: "json",
-        async:false,
-        success: function (data) {
-            console.log("第一次加载",data);
-            var prodList = data.prodList;
-            var html ="";
-            for(var i=0;i<prodList.length;i++){
-                html +='<div class="product-box " >\
-                    <a href="'+prodList[i].prodUrl+'" class="product-img">\
-                    <img src="'+prodList[i].picUrl+'" alt="'+prodList[i].title+'"/>\
-                    </a>\
-                    <div class=" product-info">\
-                    <h3 class="product-title"><a href="'+prodList[i].prodUrl+'">'+prodList[i].title+'</a></h3>\
-            <div class="buyinfo clearfix">\
-                <div class="">\
-                <div class="price"><b>￥'+prodList[i].promotionPrice+'</b></div>\
-            <div class="peopleNum">销量: '+prodList[i].buys+'  库存: '+prodList[i].stocks+'  收藏: '+prodList[i].favorite+' </div>\
-            </div>\
-            <a href="javascript:;" data-picUrl="'+prodList[i].picUrl+'" data-title="'+prodList[i].title+'" data-promotionPrice="'+prodList[i].promotionPrice+'" data-productId="'+prodList[i].productId+'" data-productUrl="'+prodList[i].prodUrl+'" class="recommendBtn main-color">推荐</a>\
-                </div>\
-                </div>\
-                </div>'
-            }
-            $('#mui-scroll').html(html);
-            if(!!callback){
-                callback(data);
-            }
-//          下拉更多
-           if(!needPage){
-               mui.init({
-                   pullRefresh: {
-                       container: "#content",//待刷新区域标识，querySelector能定位的css选择器均可，比如：id、.class等
-                       up: {
-                           height: 0,//可选.默认50.触发上拉加载拖动距离
-                           auto: false,//可选,默认false.自动上拉加载一次
-                           contentrefresh: "正在加载...",//可选，正在加载状态时，上拉加载控件上显示的标题内容
-                           contentnomore: '没有更多数据了',//可选，请求完毕若没有更多数据时显示的提醒内容；
-                           callback: function () {
-                               //业务逻辑代码，比如通过ajax从服务器获取新数据；
-                               params.curPageNO=data.curPageNO+1;
-                               $.ajax({
-                                   url: url,
-                                   type: type,
-                                   data: params,
-                                   dataType: "json",
-                                   async:false,
-                                   success: function (pageData) {
-                                       console.log("分页加载",pageData);
-                                       data.curPageNO=pageData.curPageNO;
-                                       mui('#content').pullRefresh().endPullupToRefresh(!pageData.prodList);
-                                       var pageData = pageData;
-                                       var pataHtml = template('product-box', pageData);
-                                       $('.mui-pull-bottom-pocket').before(pataHtml);
-                                       if(!!muiCallback){
-                                           muiCallback();
-                                       }
-                                   },
-                                   error: function (err) {
-                                       console.info(err)
-                                       alert(err.status+" "+err.statusText+" "+err.readyState+" "+err.responseText)
-                                       alert(JSON.stringify(err))
-                                   }
-                               })
-                               //注意：
-                               //1、加载完新数据后，必须执行如下代码，true表示没有更多数据了：
-                               //2、若为ajax请求，则需将如下代码放置在处理完ajax响应数据之后
-
-                           }//必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
-                       }
-                   }
-               });
-           }
+//分页加载
+function dropload(url,type,params,isPaging,callback){
+    if(isPaging){
+        params.curPageNO = 0;
+    }
+    $('#content').dropload({
+        scrollArea : window,
+        distance:100,
+        domDown : {
+            domClass   : 'dropload-down',
+            domRefresh : '<div class="dropload-refresh">↑上拉加载更多</div>',
+            domLoad    : '<div class="dropload-load"><span class="loading"></span>加载中...</div>',
+            domNoData  : '<div class="dropload-noData">暂无更多商品</div>'
         },
-        error: function (err) {
-            console.info(err)
-            alert(err.status+" "+err.statusText+" "+err.readyState+" "+err.responseText)
-            alert(JSON.stringify(err))
-        }
-    })
-
-}*/
-
-function ajaxSuccess (url,type,params,callback,muiCallback,needPage) {
-    $.ajax({
-        url: url,
-        type: type,
-        data: params,
-        dataType: "json",
-        async:false,
-        success: function (data) {
-            console.log("第一次加载",data);
-            var data = data;
-            var html = template('product-box', data);
-            $('#mui-scroll').html(html);
-            if(!!callback){
-                callback(data);
+        loadDownFn : function(me){
+            if(isPaging){
+                params.curPageNO++;
             }
+            $.ajax({
+                url:url ,
+                type: type,
+                data: params,
+                dataType: "json",
+                async:false,
+                success: function (data) {
+                    if(data.prodList){
+                        var data = data;
+                        var html = template('product-box', data);
+                        $('.lists').append(html);
+                        // 如果没有数据
+                    }else{
+                        // 锁定
+                        me.lock();
+                        // 无数据
+                        me.noData();
+                    }
+                    // 每次数据插入，必须重置
+                    me.resetload();
+                    if(!!callback){
+                        callback(data);
+                    }
+                },
+                error: function (err) {
+                    console.info(err)
+                    alert('Ajax error!'+JSON.stringify(err));
+                    // 即使加载出错，也得重置
+                    me.resetload();
+                }
+            })
+
+
+        }
+    });
+}
+//mui分页加载
+//function ajaxSuccess (url,type,params,callback,muiCallback,needPage) {
+//    $.ajax({
+//        url: url,
+//        type: type,
+//        data: params,
+//        dataType: "json",
+//        async:false,
+//        success: function (data) {
+//            console.log("第一次加载",data);
+//            var data = data;
+//            var html = template('product-box', data);
+//            $('#mui-scroll').html(html);
+//            if(!!callback){
+//                callback(data);
+//            }
 //          下拉更多
 //            if(!needPage){
 //                mui.init({
@@ -231,12 +200,12 @@ function ajaxSuccess (url,type,params,callback,muiCallback,needPage) {
 //                    }
 //                });
 //            }
-        },
-        error: function (err) {
-            console.info(err)
-            alert(err.status+" "+err.statusText+" "+err.readyState+" "+err.responseText)
-            alert(JSON.stringify(err))
-        }
-    })
-
-}
+//        },
+//        error: function (err) {
+//            console.info(err)
+//            alert(err.status+" "+err.statusText+" "+err.readyState+" "+err.responseText)
+//            alert(JSON.stringify(err))
+//        }
+//    })
+//
+//}
